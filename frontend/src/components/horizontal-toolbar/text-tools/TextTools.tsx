84e845fe115ccc3dafbb8ca7b231f5c958974135
Mostafa-100 <imustaphaaourik@gmail.com>
2025-03-07 14:39:19 +0000

    Refactoring text styles buttons

--- frontend/src/components/horizontal-toolbar/text-tools/TextTools.tsx
+++ frontend/src/components/horizontal-toolbar/text-tools/TextTools.tsx
@@ -1,14 +1,9 @@
-import { ArrowDown, ArrowUp, Layers } from "lucide-react";
+import { Bold, Italic, Strikethrough, Underline } from "lucide-react";
 
-import ToolButton from "../ToolButton";
 import FontSelect from "./FontSelect";
 import DeleteButton from "../shape-tools/DeleteButton";
 import CopyTextButton from "./CopyTextButton";
 import ColorInput from "./ColorInput";
-import BoldButton from "./BoldButton";
-import ItalicButton from "./ItalicButton";
-import UnderlineButton from "./UnderlineButton";
-import StrikethroughButton from "./StrikethroughButton";
 import DecreaseFontSizeButton from "./DecreaseFontSizeButton";
 import { useRef } from "react";
 import IncreaseFontSizedButton from "./IncreaseFontSizeButton";
@@ -16,8 +11,13 @@ import FontSizeInput from "./FontSizeInput";
 import OpacityButton from "../shape-tools/OpacityButton";
 import ForwardButton from "../shape-tools/ForwardButton";
 import BackwardButton from "../shape-tools/BackwardButton";
+import TextStyleButton from "./TextStyleButton";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 
 function TextTools() {
+
+  const { canvas } = useSelector((state: RootState) => state.canvas);
 
   const fontSizeInputRef = useRef<HTMLInputElement | null>(null);
 
@@ -25,10 +25,13 @@ function TextTools() {
     <div className="flex gap-x-2 items-center">
       <ColorInput />
       <FontSelect />
-      <BoldButton />
-      <ItalicButton />
-      <UnderlineButton />
-      <StrikethroughButton />
+      <TextStyleButton canvas={canvas} icon={<Bold className="h-4 w-4" />} attribute="fontWeight" defaultValue="normal" newValue="600" />
+
+      <TextStyleButton canvas={canvas} icon={<Italic className="h-4 w-4" />} attribute="fontStyle" defaultValue="normal" newValue="italic" />
+
+      <TextStyleButton canvas={canvas} icon={<Underline className="h-4 w-4" />} attribute="underline" defaultValue={false} newValue={true} />
+
+      <TextStyleButton canvas={canvas} icon={<Strikethrough className="h-4 w-4" />} attribute="linethrough" defaultValue={false} newValue={true} />
 
       <DecreaseFontSizeButton inputRef={fontSizeInputRef} />
       <FontSizeInput ref={fontSizeInputRef} />
--- frontend/src/components/vertical-toolbar/image-toolbar/Image.tsx
+++ frontend/src/components/vertical-toolbar/image-toolbar/Image.tsx
@@ -1,5 +1,7 @@
 import { showTheToolsOf } from "@/redux/horizontalToolbar";
-import { useDispatch } from "react-redux";
+import { RootState } from "@/redux/store";
+import { FabricImage } from "fabric";
+import { useDispatch, useSelector } from "react-redux";
 
 type ImageProps = {
   src: string;
@@ -8,18 +10,25 @@ type ImageProps = {
 
 function Image({ src, owner }: ImageProps) {
 
+  const { canvas } = useSelector((state: RootState) => state.canvas);
+
   const dispatch = useDispatch();
 
-  function showImageTools() {
-    dispatch(showTheToolsOf("image"));
-  }
-
+  // Do this
   function addImageToCanvas() {
-    // implement this
+    const image = new FabricImage(src, {
+      width: 300,
+      height: 200,
+    })
+
+    dispatch(showTheToolsOf("image"));
+
+    canvas?.add(image);
+    canvas?.renderAll();
   }
 
   return (
-    <div onClick={showImageTools} className="relative h-20 cursor-pointer group overflow-hidden">
+    <div onClick={addImageToCanvas} className="relative h-20 cursor-pointer group overflow-hidden">
       <img src={src} className="object-cover h-full w-full" />
       <div className="absolute opacity-0 group-hover:opacity-100 -bottom-7 group-hover:bottom-0 transition-all left-0 bg-gray-500 text-white text-xs w-full p-0.5 capitalize">{owner}</div>
     </div>
