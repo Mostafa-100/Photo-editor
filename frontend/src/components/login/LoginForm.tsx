a767422934d44b74732e5c950daa11f1fa9b1c2b
Mostafa-100 <imustaphaaourik@gmail.com>
2025-03-15 11:53:08 +0000

    Edit LoginForm.tsx

--- frontend/src/components/login/LoginForm.tsx
+++ frontend/src/components/login/LoginForm.tsx
@@ -1,16 +1,11 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useForm } from 'react-hook-form';
 
 function LoginForm() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))
-    }>
-      <Input placeholder="Email" className="mb-2" {...register("email", { required: true })} />
-      {errors.email && <div className="text-red-500">Email is required</div>}
-      <Input placeholder="Password" className="mb-2" {...register("password", { required: true })} />
-      {errors.password && <div className="text-red-500">Password is required</div>}
+    <form>
+      <Input placeholder="Email" className="mb-2" />
+      <Input placeholder="Password" className="mb-2" />
       <Button type="submit" variant="default" className="w-full cursor-pointer">Continue</Button>
     </form >
   )
